813e2715fc6d75480c18b0b72d0b98eb54326203
tenderking <gmu003@gmail.com>
2022-03-21 19:04:29 +0100

    refactor: created navbar closes #1

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,30 @@
 </template>
 <style lang="scss">
 	#app {
-		width: 100vw;
+		height: 100vh;
+		display: grid;
+		grid-template-columns: max-content auto;
+		grid-template-rows: max-content auto;
+		header {
+			grid-column: 1/-1;
+			grid-row: 1 / span 1;
+		}
+		main {
+			grid-column: 1/-1;
+			grid-row: 2 / span 1;
+			margin-inline: auto;
+		}
 	}
-	h1 {
-		list-style: none;
-
-		text-align: center;
+	@media screen and (min-width: $sz-tablet) {
+		#app {
+			header {
+				grid-column: 1 / span 1;
+				grid-row: 1 / -1;
+			}
+			main {
+				grid-column: 2/-1;
+				grid-row: 1/ -1;
+			}
+		}
 	}
 </style>
