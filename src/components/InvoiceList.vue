b0e745451f2c87a0ff86da356a1ce7d6c3428bc3
tenderking <gmu003@gmail.com>
2022-03-31 00:48:26 +0200

    refactor: #3

---
+++ src/components/InvoiceList.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="wrapper-1">
+		<InvoicesListNav />
+	</div>
+	<div class="wrapper-1">
+		<template v-for="item in store.filteredInvoice">
+			<InvoicesListItem :item="item" />
+		</template>
+	</div>
+</template>
+<script lang="ts" setup>
+	import { useInvoiceStore } from "@/stores/invoice";
+	import InvoicesListItem from "./InvoicesListItem.vue";
+	import InvoicesListNav from "./InvoicesListNav.vue";
+
+	const store = useInvoiceStore();
+</script>
