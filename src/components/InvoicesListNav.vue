0799e44c8dd0394300a787fe88a31aebf4ef2c57
tenderking <gmu003@gmail.com>
2022-03-31 00:48:52 +0200

    refactor: #4

---
+++ src/components/InvoicesListNav.vue
@@ -0,0 +1,76 @@
+<script lang="ts" setup>
+	import { ref } from "vue";
+	import ButtonNewInvoice from "./buttons/ButtonNewInvoice.vue";
+	import IconArrowDown from "./icons/IconArrowDown.vue";
+	const filterBox = ref(false);
+	const showFilters = () => (filterBox.value = !filterBox.value);
+</script>
+<template>
+	<div class="nav p-2">
+		<div>
+			<h2>Invoices</h2>
+			<p>Invoices</p>
+		</div>
+
+		<div class="filter-dd" @click="showFilters()">
+			<h4>Filter</h4>
+			<i><IconArrowDown /></i>
+		</div>
+		<div class="filter shadow-filter br-8" v-show="filterBox">
+			<div class="filter-item">
+				<input type="checkbox" name="paid" id="paid" />
+				<label for="paid">
+					<h4>Paid</h4>
+				</label>
+			</div>
+
+			<div class="filter-item">
+				<input type="checkbox" name="pending" id="pending" />
+				<label for="pending">
+					<h4>Pending</h4>
+				</label>
+			</div>
+			<div class="filter-item">
+				<input type="checkbox" name="draft" id="draft" />
+				<label for="draft">
+					<h4>Draft</h4>
+				</label>
+			</div>
+		</div>
+
+		<ButtonNewInvoice />
+	</div>
+</template>
+<style lang="scss" scoped>
+	.nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		position: relative;
+		.filter-dd {
+			display: flex;
+			gap: 1rem;
+			margin-left: auto;
+			padding: 1rem;
+		}
+		.filter {
+			position: absolute;
+			top: 5rem;
+			right: 12rem;
+			z-index: 10;
+			background-color: var(--color-background-mute);
+			width: max-content;
+			padding: 1rem;
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			gap: 0.5rem;
+
+			&-item {
+				display: flex;
+				align-items: center;
+				gap: 1rem;
+			}
+		}
+	}
+</style>
