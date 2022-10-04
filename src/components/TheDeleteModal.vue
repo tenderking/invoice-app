7e1e3a0f6118a8f93e751a1377e69c29775d6f43
tenderking <gmu003@gmail.com>
2022-10-04 14:08:17 +0200

    feat: closes TheModalDelete #14

--- src/components/TheDeleteModal.vue
+++ src/components/TheDeleteModal.vue
@@ -1,15 +1,67 @@
 <template>
-  <dialog ref="dialog">
-    <h2>Are you sure you want to Delete</h2>
-    <button>Cancel</button>
-    <button>Delete</button>
-  </dialog>
+	<dialog ref="dialog">
+		<div class="modal__container">
+			<h2>Confirm Deletion</h2>
+			<p>
+				Are you sure you want to delete invoice #{{ id }} This action cannot be
+				undone.
+			</p>
+			<div class="flex-flow-2">
+				<ButtonCancel @click="closeModal">Cancel</ButtonCancel>
+				<ButtonDelete>Delete</ButtonDelete>
+			</div>
+		</div>
+	</dialog>
 </template>
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+	import { onMounted, ref } from "vue";
+	import ButtonCancel from "./ButtonCancel.vue";
+	import ButtonDelete from "./ButtonDelete.vue";
 
-const dialog = ref<HTMLDialogElement | null>(null);
-onMounted(() => {
-  dialog.value?.showModal();
-});
+	const closeModal = () => emits("update:isShowModal", !props.isShowModal);
+	const props = defineProps({
+		isShowModal: {
+			type: Boolean,
+			required: true,
+		},
+		id: {
+			type: String,
+			required: true,
+		},
+	});
+	const emits = defineEmits(["update:isShowModal"]);
+	const dialog = ref<HTMLDialogElement | null>(null);
+	onMounted(() => {
+		dialog.value?.showModal();
+	});
 </script>
+<style lang="scss">
+	dialog {
+		width: 100%;
+		height: 100%;
+		display: grid;
+		align-items: center;
+		outline: none;
+		border: none;
+
+		background-color: var(--overlay);
+		.modal__container {
+			background-color: var(--color-background);
+
+			box-shadow: $shadow-invoice;
+			color: var(--color-text);
+			padding: $pd-mb-modal;
+			border-radius: $radius-invoice;
+			width: fit-content;
+			margin: auto;
+			h2 {
+				padding-bottom: $pd-mb-modal;
+			}
+			p {
+				color: var(--color-text-soft);
+				padding-bottom: $pd-mb-modal;
+				max-width: 40ch;
+			}
+		}
+	}
+</style>
--- src/components/ViewInvoiceNav.vue
+++ src/components/ViewInvoiceNav.vue
@@ -1,100 +1,105 @@
 <script setup lang="ts">
-import ButtonEdit from "./ButtonEdit.vue";
-import ButtonDelete from "./ButtonDelete.vue";
-import ButtonMarkAsPaid from "./ButtonMarkAsPaid.vue";
-import { useInvoiceStore } from "@/stores/invoiceStore";
-import { computed, ref } from "vue";
-import TheDeleteModal from "./TheDeleteModal.vue";
-const store = useInvoiceStore();
+	import ButtonEdit from "./ButtonEdit.vue";
+	import ButtonDelete from "./ButtonDelete.vue";
+	import ButtonMarkAsPaid from "./ButtonMarkAsPaid.vue";
+	import { useInvoiceStore } from "@/stores/invoiceStore";
+	import { computed, ref } from "vue";
+	import TheDeleteModal from "./TheDeleteModal.vue";
+	const store = useInvoiceStore();
 
-const props = defineProps({
-  invoiceId: {
-    type: String,
-    required: true,
-  },
-});
+	const props = defineProps({
+		invoiceId: {
+			type: String,
+			required: true,
+		},
+	});
 
-const getStatus = computed(
-  () =>
-    store.getInvoices.find((invoice) => invoice.id === props.invoiceId)?.status
-);
-const statusColors = computed(() => {
-  let textColor = "";
-  let bgColor = "";
-  if (getStatus.value === "paid") {
-    textColor = "hsl(160, 67%, 52%)";
-    bgColor = "hsla(160, 67%, 52%, 0.057)";
-  }
-  return {
-    "--text-color": textColor,
-    "--bg-color": bgColor,
-  };
-});
-const showDialog = ref(false);
-function openDialog() {
-  showDialog.value = true;
-}
+	const getStatus = computed(
+		() =>
+			store.getInvoices.find((invoice) => invoice.id === props.invoiceId)
+				?.status
+	);
+	const statusColors = computed(() => {
+		let textColor = "";
+		let bgColor = "";
+		if (getStatus.value === "paid") {
+			textColor = "hsl(160, 67%, 52%)";
+			bgColor = "hsla(160, 67%, 52%, 0.057)";
+		}
+		return {
+			"--text-color": textColor,
+			"--bg-color": bgColor,
+		};
+	});
+	const isShowModal = ref(false);
+	function openDialog() {
+		isShowModal.value = true;
+	}
 </script>
 <template>
-  <div class="main wrapper-1 p-1 br-8 flex-flow">
-    <div :style="statusColors" class="status flex-flow">
-      <p class="status-text">Status</p>
-      <h4>{{ getStatus }}</h4>
-    </div>
-    <div class="cta-buttons flex-flow">
-      <router-link :to="{ name: 'InvoicesEdit', params: { id: invoiceId } }">
-        <ButtonEdit />
-      </router-link>
+	<div class="main wrapper-1 p-1 br-8 flex-flow">
+		<div :style="statusColors" class="status flex-flow">
+			<p class="status-text">Status</p>
+			<h4>{{ getStatus }}</h4>
+		</div>
+		<div class="cta-buttons flex-flow">
+			<router-link :to="{ name: 'InvoicesEdit', params: { id: invoiceId } }">
+				<ButtonEdit />
+			</router-link>
 
-      <ButtonDelete @click="openDialog" />
-      <ButtonMarkAsPaid @click="store.setStatusAsPaid(invoiceId)" />
-    </div>
-  </div>
-  <TheDeleteModal v-if="showDialog" />
+			<ButtonDelete @click="openDialog" />
+			<ButtonMarkAsPaid @click="store.setStatusAsPaid(invoiceId)" />
+		</div>
+	</div>
+	<TheDeleteModal
+		v-if="isShowModal"
+		v-model:isShowModal="isShowModal"
+		:id="invoiceId"
+	/>
 </template>
 <style lang="scss" scoped>
-.main {
-  position: relative;
-  background-color: var(--color-background-mute);
+	.main {
+		position: relative;
+		background-color: var(--color-background-mute);
 
-  .status {
-    --text-color: var(--warning-60);
-    --bg-color: var(--warning-80);
+		.status {
+			--text-color: var(--warning-60);
+			--bg-color: var(--warning-80);
 
-    width: 100%;
-    &-text {
-      color: var(--color-text-soft);
-    }
-  }
-  h4 {
-    background-color: var(--bg-color);
-    color: var(--text-color);
+			width: 100%;
+			&-text {
+				color: var(--color-text-soft);
+			}
+		}
+		h4 {
+			background-color: var(--bg-color);
+			color: var(--text-color);
 
-    border-radius: $radius-invoice;
-    padding: 1rem;
-  }
-  .cta-buttons {
-    position: fixed;
+			border-radius: $radius-invoice;
+			padding: 1rem;
+		}
+		.cta-buttons {
+			position: fixed;
 
-    bottom: 0;
-    left: 0;
-    background-color: var(--color-background-mute);
-    width: 100%;
-    padding: 1rem;
-  }
-}
-@media screen and (min-width: $sz-tablet) {
-  .main {
-    .status {
-      width: 25%;
-    }
-    .cta-buttons {
-      position: static;
-      width: auto;
+			bottom: 0;
+			left: 0;
+			background-color: var(--color-background-mute);
+			width: 100%;
+			padding: 1rem;
+		}
+	}
+	@media screen and (min-width: $sz-tablet) {
+		.main {
+			.status {
+				width: 25%;
+			}
+			.cta-buttons {
+				position: static;
+				width: auto;
 
-      gap: 1em;
-      padding: 0;
-    }
-  }
-}
+				gap: 1em;
+				padding: 0;
+			}
+		}
+	}
 </style>
